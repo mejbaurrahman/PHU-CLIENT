2b1bcf7d5052d23ebf6293809af8ef2825e7504c
unknown <mejbacsedu@gmail.com>
2024-07-13 23:38:51 +0600

    sidebar

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,7 +1,6 @@
-import { Layout, Menu } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminPaths } from "../../routes/admin-routes";
-import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import Sidebar from "./Sidebar";
 
 // import {
 //   UploadOutlined,
@@ -10,7 +9,7 @@ import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 // } from "@ant-design/icons";
 // import React from "react";
 
-const { Content, Footer, Header, Sider } = Layout;
+const { Content, Footer, Header } = Layout;
 // const items = [
 //   UserOutlined,
 //   VideoCameraOutlined,
@@ -46,39 +45,11 @@ const { Content, Footer, Header, Sider } = Layout;
 //     ],
 //   },
 // ];
-const items = sidebarItemsGenerator(adminPaths, "admin");
+
 export default function MainLayout() {
   return (
     <Layout style={{ height: "100vh" }}>
-      <Sider
-        breakpoint="lg"
-        collapsedWidth="0"
-        onBreakpoint={(broken) => {
-          console.log(broken);
-        }}
-        onCollapse={(collapsed, type) => {
-          console.log(collapsed, type);
-        }}
-      >
-        <div
-          style={{
-            color: "white",
-            textAlign: "center",
-            height: "4rem",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <h1>PH UNIVERSITY</h1>
-        </div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={["4"]}
-          items={items}
-        />
-      </Sider>
+      <Sidebar></Sidebar>
       <Layout>
         <Header style={{ padding: 0 }} />
         <Content style={{ margin: "24px 16px 0" }}>
